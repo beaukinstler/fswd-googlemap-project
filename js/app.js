3a2845ecb6e05a8d381145a0f6bb77128677019f
Beau Kinstler <git@beaukinstler.info>
2018-04-11 23:58:14 -0400

    Adding nick name voting
    this was quite a learning experience around how JS and ko
    work.  First off, I had the idea to allow clicking on the nick
    names, expanding the quiz concept, to increment a counter for the
    nick names as well, like voting.  However, it proved much more dificult
    than the example I was using from KO's website.  In the foreach
    functionality of the observeable array, I tried a number of things,
    changing this to self in the main code, so I could use this for the
    object getting passed back from the click in the foreach loop,
    using other kinds of functions to increment the clicks, ultimately
    leanrning that observeableArrays don't update only if their
    properties update. So, I then used the replace function, but also
    had to allow the replace function to see the orignal, and make a copy of
    the object to increase the clicks.  Ultimlatly, that's where I landed.

---
+++ js/app.js
@@ -0,0 +1,77 @@
+var ViewModel = function() {
+
+    var self = this;
+
+    self.clickCount = ko.observable(0);
+    self.clickNickCount = ko.observable(0);
+    self.name = ko.observable('Tabby');
+    self.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    self.imgAttr = ko.observable('');
+
+    self.level = ko.computed(
+        function(){
+            switch (true) {
+                case (self.clickCount() < 10):
+                  return "Meh...."
+                  break;
+                case (self.clickCount() < 20):
+                  return "Lovable"
+                  break;
+                case (self.clickCount() < 30):
+                  return "Irresistible";
+                  break;
+                default:
+                  return "Help!!! Stop!!"
+            }}
+        ,self);
+
+    self.incrementCounter = function() {
+        self.clickCount(self.clickCount() + 1);
+    };
+
+    self.incrementNickCounter = function() {
+        self.clickNickCount(self.clickNickCount() + 1);
+    };
+
+    self.changeFirstNick = function() {
+        self.nicknames()[0].nick = "Ted";
+    };
+
+
+    // data
+
+    self.addNick = function() {
+        self.nicknames.push({ nick: "newname",nickClicks:0 });
+    };
+
+    // function addAClick(obj){
+    //     obj.nickClicks +=1;
+    //     return obj;
+    // };
+
+    self.nickClicksAdd = function(){
+        console.log(this);
+        var temp = Object.create(this);
+        temp.nickClicks += 1;
+        self.nicknames.replace(this,temp);
+        console.log(self.nicknames());
+        // for (name in self.nicknames()){
+        //     console.log(self.nicknames()[name].nick == this.nick)
+        //     if (self.nicknames()[name].nick == this.nick){
+        //         console.log("match" )
+        //         console.log(self.nicknames()[name].nickClicks);
+        //         self.nicknames()[name].nickClicks += 1;
+        //         console.log(self.nicknames()[name].nickClicks);
+        //     }
+        // }
+    }
+    self.nicknames = ko.observableArray([
+            {"nick":'Bert',"nickClicks":3},
+            {"nick":'Charles',"nickClicks":0},
+            {"nick":'Denise',"nickClicks":0}
+        ]);
+
+}
+
+ko.applyBindings(new ViewModel());
+
