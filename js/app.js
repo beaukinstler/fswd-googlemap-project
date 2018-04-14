6ca3b2f48be2adab93c5c0f7cff9f0826444b713
Beau Kinstler <git@beaukinstler.info>
2018-04-14 14:26:26 -0400

    Change view to use with
    had to chang view model to use the 'self' copy of this.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,38 @@
 var ViewModel = function() {
 
     var self = this;
+    self.currentCat =   ko.observable(new Cat());
+
+    self.incrementCounter = function() {
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };
+
+    // this.incrementNickCounter = function() {
+    //     this.currentCat().clickNickCount(this.currentCat().clickNickCount() + 1);
+    // };
+
+    // this.changeFirstNick = function() {
+    //     this.currentCat().nicknames()[0].nick = "Ted";
+    // };
+
+
+
+
+    // this.addNick = function() {
+    //     this.currentCat().nicknames.push({ nick: "newname",nickClicks:0 });
+    // };
+
+    // function addAClick(obj){
+    //     obj.nickClicks +=1;
+    //     return obj;
+    // };
+
+
+}
+
+
+var Cat = function(){
+    var self = this;
 
     self.clickCount = ko.observable(0);
     self.clickNickCount = ko.observable(0);
@@ -25,31 +57,7 @@ var ViewModel = function() {
             }}
         ,self);
 
-      
 
-    self.incrementCounter = function() {
-        self.clickCount(self.clickCount() + 1);
-    };
-
-    self.incrementNickCounter = function() {
-        self.clickNickCount(self.clickNickCount() + 1);
-    };
-
-    self.changeFirstNick = function() {
-        self.nicknames()[0].nick = "Ted";
-    };
-
-
-    // data
-
-    self.addNick = function() {
-        self.nicknames.push({ nick: "newname",nickClicks:0 });
-    };
-
-    // function addAClick(obj){
-    //     obj.nickClicks +=1;
-    //     return obj;
-    // };
 
     self.nickClicksAdd = function(){
         console.log(this);
@@ -57,26 +65,19 @@ var ViewModel = function() {
         temp.nickClicks += 1;
         self.nicknames.replace(this,temp);
         console.log(self.nicknames());
-        // for (name in self.nicknames()){
-        //     console.log(self.nicknames()[name].nick == this.nick)
-        //     if (self.nicknames()[name].nick == this.nick){
-        //         console.log("match" )
-        //         console.log(self.nicknames()[name].nickClicks);
-        //         self.nicknames()[name].nickClicks += 1;
-        //         console.log(self.nicknames()[name].nickClicks);
-        //     }
-        // }
+
     }
     self.nicknames = ko.observableArray([
-            {"nick":'Bert',"nickClicks":3},
-            {"nick":'Charles',"nickClicks":0},
-            {"nick":'Denise',"nickClicks":0}
-        ]);
+        {"nick":'Bert',"nickClicks":3},
+        {"nick":'Charles',"nickClicks":0},
+        {"nick":'Denise',"nickClicks":0}
+    ]);
 
     self.topNickName = ko.computed(function() {
         return self.nicknames().sort(function (left, right) { return left.nickClicks == right.nickClicks ? 0 : (left.nickClicks < right.nickClicks ? 1 : 0)})[0].nick ;
-    }, self);  
+    }, self);        
 }
+
 
 ko.applyBindings(new ViewModel());
 
