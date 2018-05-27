971db0fbb56a9287c80e4af9fd23b1694101069d
Beau Kinstler <git@beaukinstler.info>
2018-05-26 23:17:48 -0400

    Move data to a separate data.js file

--- js/app.js
+++ js/app.js
@@ -45,65 +45,6 @@ var NYTApi = function(searchTerm,domErrorDiv,infowindow){
                 });
 }
 
-// Create the initial search groups.  This list could be added to.
-var initialSearches = [
-    {
-        "name":"Filter List 1",
-        "searchterms":[
-            ]
-    },
-    {
-        "name":"Filter List 2",
-        "searchterms":[
-            ]
-    }
-]
-
-// Create the markers used in the App.
-var defaultMarkers = [
-    {
-        "addressString": "",
-        "name": "Dimattina Playground",
-        "lat": 40.680311,
-        "lng": -74.002211
-    },
-    {
-        "addressString": "",
-        "name": "Brooklyn Bridge Park",
-        "lat": 40.700291,
-        "lng": -73.996699
-    },
-    {
-        "addressString": "",
-        "name": "Cadman Plaza Park",
-        "lat": 40.698476,
-        "lng": -73.990469
-    },
-    {
-        "addressString": "",
-        "name": "Trinity Park",
-        "lat": 40.699344,
-        "lng": -73.984105
-    },
-    {
-        "addressString": "",
-        "name": "Prospect Park",
-        "lat": 40.660204,
-        "lng": -73.968956
-    },
-    {
-        "addressString": "",
-        "name": "Herbert Von King Park",
-        "lat": 40.689612,
-        "lng": -73.946732
-    },
-    {
-        "addressString": "",
-        "name": "Jacob Riis Park",
-        "lat": 40.567070,
-        "lng": -73.871770
-    }
-]
 
 // Below are some help
 // var namesOfMarkers = defaultMarkers.map(function(markerToConvert) {
---
+++ js/data.js
@@ -0,0 +1,61 @@
+'use strict'
+
+// Create the initial search groups.  This list could be added to.
+var initialSearches = [
+    {
+        "name":"Filter List 1",
+        "searchterms":[
+            ]
+    },
+    {
+        "name":"Filter List 2",
+        "searchterms":[
+            ]
+    }
+]
+
+// Create the markers used in the App.
+var defaultMarkers = [
+    {
+        "addressString": "",
+        "name": "Dimattina Playground",
+        "lat": 40.680311,
+        "lng": -74.002211
+    },
+    {
+        "addressString": "",
+        "name": "Brooklyn Bridge Park",
+        "lat": 40.700291,
+        "lng": -73.996699
+    },
+    {
+        "addressString": "",
+        "name": "Cadman Plaza Park",
+        "lat": 40.698476,
+        "lng": -73.990469
+    },
+    {
+        "addressString": "",
+        "name": "Trinity Park",
+        "lat": 40.699344,
+        "lng": -73.984105
+    },
+    {
+        "addressString": "",
+        "name": "Prospect Park",
+        "lat": 40.660204,
+        "lng": -73.968956
+    },
+    {
+        "addressString": "",
+        "name": "Herbert Von King Park",
+        "lat": 40.689612,
+        "lng": -73.946732
+    },
+    {
+        "addressString": "",
+        "name": "Jacob Riis Park",
+        "lat": 40.567070,
+        "lng": -73.871770
+    }
+]
